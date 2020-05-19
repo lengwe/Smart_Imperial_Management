46a8e36eb3b047dc29d6cdc441d338cbfce7d99c
lengwe <wl4817@ic.ac.uk>
2020-05-19 20:31:35 +0100

    login, logout, signin work

---
+++ src/app/app.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { Platform } from 'ionic-angular';
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import Parse from 'parse';
+
+@Component({
+  templateUrl: 'app.html'
+})
+export class MyApp {
+  rootPage:any;
+
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+    platform.ready().then(() => {
+      // Okay, so the platform is ready and our plugins are available.
+      // Here you can do any higher level native things you might need.
+      statusBar.styleDefault();
+      splashScreen.hide();
+
+      Parse.initialize("XiiCvvh3t61q2vMSQornvYKPPsPcTszpsMB0tpbT", "UMEsWQpchNtjV5XCyaA6uXaZOAiXO5x73vObhzKv");
+      Parse.serverURL = 'https://parseapi.back4app.com/';
+
+      Parse.User.currentAsync().then(user => {
+        console.log('Logged user', user);
+
+        this.rootPage = user ? 'HomePage' : 'LoginPage';
+      }, err => {
+        console.log('Error getting logged user');
+
+        this.rootPage = 'LoginPage';
+      })
+    });
+  }
+}
---
+++ src/pages/login/login.ts
@@ -0,0 +1,65 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, ToastController } from 'ionic-angular';
+import Parse from 'parse';
+/**
+ * Generated class for the LoginPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-login',
+  templateUrl: 'login.html',
+})
+export class LoginPage {
+  username:string;
+  password:string;
+
+  constructor(  public navCtrl: NavController,public toastCtrl: ToastController,) {
+  }
+
+  signUp() {
+    Parse.User.signUp(this.username, this.password).then((resp) => {
+      console.log('Logged in successfully', resp);
+
+      // Clears up the form
+      this.username = '';
+      this.password = '';
+
+      this.toastCtrl.create({
+        message: 'Account created successfully',
+        duration: 2000
+      }).present();
+    }, err => {
+      console.log('Error signing in', err);
+
+      this.toastCtrl.create({
+        message: err.message,
+        duration: 2000
+      }).present();
+    });
+  }
+
+  signIn() {
+    Parse.User.logIn(this.username, this.password,{}).then((resp) => {
+      console.log('Logged in successfully', resp);
+
+      // If you app has Tabs, set root to TabsPage
+      this.navCtrl.setRoot('HomePage')
+    }, err => {
+      console.log('Error logging in', err);
+
+      this.toastCtrl.create({
+        message: err.message,
+        duration: 2000
+      }).present();
+    });
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad LoginPage');
+  }
+
+}
